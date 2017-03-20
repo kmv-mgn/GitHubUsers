94f9829f01721478bb77a9aeceb4b91d92dc5ba7
kmv_work <kmv_mgn@mail.ru>
2017-03-20 16:05:27 +0500

    first commit

---
+++ app/src/main/java/com/example/kmv/githubusers/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.kmv.githubusers;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private static final String TAG = "MyLog";
+    private static final String TAG_Log = "TAG_Log";
+
+    /*@ViewPager, в котором будет размещаться содержание раздела.*/
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
