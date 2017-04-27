9e490864a80820e9d99e5c1ca163748302b6bba4
kmv_work <kmv_mgn@mail.ru>
2017-04-27 16:11:23 +0500

    finishObservable27042017

--- app/src/main/java/com/example/kmv/githubusers/activity/MainActivity.java
+++ app/src/main/java/com/example/kmv/githubusers/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.kmv.githubusers.GitHubService;
 import com.example.kmv.githubusers.GitHubServiceUserInfo;
@@ -38,8 +39,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG_Log = "TAG_Log";
     private static final String URL = "https://api.github.com/";    //базовый URL
 
-    private ArrayList<String> userList = new ArrayList<String>();    //для хранения списка логинов пользователей
-    //private List<User> users = new ArrayList<User>();                //для хранения полученного с сервера списка объектов типа User
+    private List<User> usersList = new ArrayList<User>();                //для хранения полученного с сервера списка объектов типа User
 
         private Gson gson = new GsonBuilder().create();                 //Инициализация объекта Gson
 
@@ -77,13 +77,23 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
 //---------------------------------------------------------------------------------------------------
-       /*Observable<ArrayList<User>> usersGitHub = gitHubService.getUsers();      //получили список пользователей с апи
-        usersGitHub.subscribeOn(Schedulers.newThread())
-                    .subscribe();*/
-       // usersGitHub();
-      //  userInfo("kmv-mgn");
-      //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-       exampleUserInfo();
+        //такой вариант:
+        usersList.clear();
+        exampleUserInfo();      //получаем список польз-лей, а потом полную информацию о каждом по логину из списка
+         for (User user : usersList){
+            Log.d(TAG,"У пользователя "+user.getLogin()+" подписчиков = "+user.getFollowers());
+        }
+
+ /*       //или такой вариант:
+        usersList.clear();
+        getUserListGitHub();    //получаем список пользователей с api
+
+        for (User user : usersList){
+            Log.d(TAG,"usersList:  Человечек из списка: "+user.getLogin()+" = "+user.getFollowers());
+            getUserInfoGitHub(user.getLogin());     //получаем полную информацию о пользователе по логину
+        }
+        Toast.makeText(this, "Конец!", Toast.LENGTH_LONG).show();
+*/
 
 
 
@@ -93,72 +103,99 @@ public class MainActivity extends AppCompatActivity {
     //--------------------------------------------------------------------------------
     //  ------------              мои методы            ------------------------------
     //--------------------------------------------------------------------------------
-/*
-    private Observable<ArrayList<User>> usersGitHub(){          //получаем объект Observable от апи со списком
-                                                                //всех открытых в апи пользователей
-        Observable<ArrayList<User>> usersGitHub = gitHubService.getUsers();
+//для варианта 2
+    private Observable<ArrayList<User>> getUserListGitHub(){        //Получение списка пользователей от api
+        //информацией по конкретному пользователю
 
-        usersGitHub.subscribeOn(Schedulers.newThread())        //подписываем все действия в отдельный поток
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<ArrayList<User>>() {
-                    @Override
-                    public void call(ArrayList<User> users) {
-                        for (User user: users) {
-                            Log.d(TAG, "Логин, который получили с апи" + user.getLogin());
-                        }
-                    }
-                });
-        return usersGitHub;
+        Observable<ArrayList<User>> userList = gitHubService.getUsers();  //Получает список пользователей с небольшим набором свойств пользователей
+        Subscriber<ArrayList<User>> subscriber = new Subscriber<ArrayList<User>>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG,"----Выполнение метода onCompleted");
+            }
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG,"-----Ошибка onError: "+e);
+            }
+            @Override
+            public void onNext(ArrayList<User> users) {
+                Log.d(TAG,"-----Проверка первого элемента: "+users.get(1).getLogin()+" = "+users.get(1).getFollowers());
+                usersList = users;
+            }
+        };
+        userList.subscribe(subscriber);
+        return userList;
     }
-
-    private Observable<User> userInfo(String userLogin){        //получаем объект Observable от апи с
-                                                                //информацией по конкретному пользователю
+//для варианта 2
+    private Observable<User> getUserInfoGitHub(final String userLogin){        //Получение информации о пользователе
+        //информацией по конкретному пользователю
         Observable<User> userInfo = gitHubServiceUserInfo.getUserInfo(userLogin);
-       userInfo.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<User>() {
-                    @Override
-                    public void call(User user) {
-                        Log.d(TAG,"Количество подписчиков"+user.getFollowers());
-                    }
-                });
+        Subscriber<User> subscriber = new Subscriber<User>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG,"-+-+-Выполнение метода onCompleted");
+            }
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG,"-+-+-Ошибка onError: "+e);
+            }
+            @Override
+            public void onNext(User user) {
+                Log.d(TAG,"-+-+-Количество подписчиков у "+user.getLogin()+" = "+user.getFollowers());
+                //usersList = users;
+            }
+        };
+        userInfo.subscribe(subscriber);
         return userInfo;
     }
-*/
-    private Observable<ArrayList<User>> exampleUserInfo(){        //Общий Observable от апи с
+
+//для варианта 1
+  private Observable<ArrayList<User>> exampleUserInfo(){        //Получение информации о пользователях от api
         //информацией по конкретному пользователю
-        Observable<ArrayList<User>> exampleUserInfo = gitHubService.getUsers();  //Получает список пользователей с данными
+        Observable<ArrayList<User>> exampleUserInfo = gitHubService.getUsers();  //Получает список пользователей с небольшим набором свойств пользователей
         exampleUserInfo
                 .flatMap(new Func1<List<User>, Observable<User>>() {
                     @Override
-                    public Observable<User> call(List<User> users) {
+                    public Observable<User> call(List<User> users) {                    //преобразуем каждый "выпущенный" User в отдельный observable
                         return Observable.from(users);
                     }
                 })
-                .flatMap(new Func1<User, Observable<User>>() {
+                .concatMap(new Func1<User, Observable<User>>() {
                     @Override
                     public Observable<User> call(User user) {
-                        return gitHubServiceUserInfo.getUserInfo(user.getLogin());            //userInfo(user.getLogin());
+                        return gitHubServiceUserInfo.getUserInfo(user.getLogin());  //получаем информацию о пользователе по его логину
+
                     }
                 })
-                .subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnError(new Action1<Throwable> ()
                         {
                             @Override
                             public void call(Throwable throwable) {
-                                onError();
+                                Log.d(TAG,"Ошибка  "+ throwable);
+                                //onError();
                             }
                         })
-                .subscribe(new Action1<User>() {
+                .subscribe(new Subscriber<User>() {
                     @Override
-                    public void call(User user) {
-                        Log.d(TAG,"Количество подписчиков у "+user.getLogin()+" = "+user.getFollowers());
+                    public void onCompleted() {
+                        Log.d(TAG,"-+-+-Выполнение метода onCompleted");
                     }
-                    });
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG,"-+-+-Ошибка onError: "+e);
+                    }
+                    @Override
+                    public void onNext(User user) {
+                        Log.d(TAG,"Количество подписчиков у "+user.getLogin()+" = "+user.getFollowers());
+                        usersList.add(user);
+                    }
+                });
 
         return exampleUserInfo;
     }
+
 
     private void onError() {
         Log.d(TAG,"Ууууупсс.. ошибочка вышла..");
